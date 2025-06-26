4286f415f0fbc6e2d52fef8fd6b7c48732f8f9a8
guevaraeduard <guevaraeduard97@gmail.com>
2025-06-26 14:04:54 -0400

    acomodando importaciones

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { User, UserSchema } from 'src/entities/user.entity';
+import { User, UserSchema } from '../entities/user.entity';
 
 @Module({
   controllers: [AuthController],
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { User } from 'src/entities/user.entity';
+import { User } from '../entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 @Injectable()
 export class AuthService {
